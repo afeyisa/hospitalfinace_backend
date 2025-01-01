9e9eb2f97dac050a787e6faf36ce33cce3853549
afeyisa <kenenisafeyisa@gmail.com>
2025-01-01 09:50:19 +0300

    removed static file server route

--- server.js
+++ server.js
@@ -18,19 +18,6 @@
     server.use(express.json());
     // updated  
     server.use(finance);
-    // the path where the finance frontend page going to be served 
-    // on my divece the page path = ../softwareProject/my-app/build'
-    server.use(express.static(path.join(__dirname,'../softwareProject/my-app/build')));  
-    // for this part i think you can create your own router for each pages 
-    server.get(['/finance','/paidsalry','/sold-products','/salary-payment','/expense','/revenue','/pay-expense','/sold-products','/hospitalService'], (req, res) => {
-        try{
-        res.sendFile(path.join(__dirname,'../softwareProject/my-app/build','index.html'));}
-        catch{
-            res.status(404).send('server error');
-        }
-    });
-
-
     mongoose.connect(process.env.database).then(()=>console.log('data base connected'));
     const port= process.env.PORT;
     server.listen(port,()=>{   
